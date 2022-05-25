29db7592cea44fd6d51367b8ac6694c76eb5d97a
Artem Bulkhak <artem.bulkhak@mail.ru>
2022-05-25 12:26:00 +0300

    Initial Commit

---
+++ Todoey-iOS-BootCamp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Todoey-iOS-BootCamp
+//
+//  Created by Artem Bulkhak on 25.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
